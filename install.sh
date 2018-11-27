56249c0f16e938c7144b289405844caf3ad965c7
jldeon <jldeon@users.noreply.github.com>
2018-11-27 15:48:42 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+function insert_line () {
+  LINE=$1
+  FILE=$2
+  grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+}
+
+# tmux
+cd $HOME
+git clone https://github.com/gpakosz/.tmux
+ln -s -f .tmux/.tmux.conf
+ln -s -f $DIR/tmux .tmux.conf.local
+
+# bash
+insert_line "source $DIR/bash" "$HOME/.bashrc"
+
+echo "Done!"

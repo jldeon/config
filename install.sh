c5268d82b61526669805238466c3d07acb8384de
jldeon <jdeonarine@gmail.com>
2018-11-27 16:17:47 -0600

    Move some files around to avoid conflicts. Adjust tabstops

--- install.sh
+++ install.sh
@@ -5,21 +5,23 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 function insert_line () {
-  LINE=$1
-  FILE=$2
-  grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+    LINE=$1
+    FILE=$2
+    grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
 }
 
 # tmux
 cd $HOME
-git clone https://github.com/gpakosz/.tmux
-ln -s -f .tmux/.tmux.conf
-ln -s -f $DIR/tmux $HOME/.tmux.conf.local
+if [[ ! -d '.tmux' ]]; then
+    git clone https://github.com/gpakosz/.tmux
+    ln -s -f .tmux/.tmux.conf
+fi
+ln -s -f $DIR/tmux.cfg $HOME/.tmux.conf.local
 
 # nano
-ln -s -f $DIR/nano $HOME/.nanorc
+ln -s -f $DIR/nano.cfg $HOME/.nanorc
 
 # bash
-insert_line "source $DIR/bash" "$HOME/.bashrc"
+insert_line "source $DIR/bash.cfg" "$HOME/.bashrc"
 
 echo "Done!"
